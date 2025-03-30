4ef5e4dded8d6dcfc2476dcc6facb9cd34a3a530
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-03-30 14:59:55 +0800

    useReducer

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,27 +1,37 @@
-// import ComponentA from "./hooks/useContext/ComponentA";
-// import { createContext } from "react";
-// export const Data = createContext();
-// export const Data1 = createContext();
+// // import ComponentA from "./hooks/useContext/ComponentA";
+// // import { createContext } from "react";
+// // export const Data = createContext();
+// // export const Data1 = createContext();
 
-import UpdateUser from "./hooks/useContext/exercises/UpdateUser";
-import { UserProvider } from "./hooks/useContext/exercises/UserContext";
-import UserProfile from "./hooks/useContext/exercises/UserProfile";
+// // import UpdateUser from "./hooks/useContext/exercises/UpdateUser";
+// // import { UserProvider } from "./hooks/useContext/exercises/UserContext";
+// // import UserProfile from "./hooks/useContext/exercises/UserProfile";
 
-const App = () => {
-  // const name = "C_11nJxx";
-  // const age = 21;
-  // return (
-  // <Data.Provider value={name}>
-  //   <Data1.Provider value={age}>
-  //     <ComponentA />
-  //   </Data1.Provider>
-  // </Data.Provider>
-  // );
+// const App = () => {
+//   // const name = "C_11nJxx";
+//   // const age = 21;
+//   // return (
+//   // <Data.Provider value={name}>
+//   //   <Data1.Provider value={age}>
+//   //     <ComponentA />
+//   //   </Data1.Provider>
+//   // </Data.Provider>
+//   // );
+//   //   return (
+//   //     <UserProvider>
+//   //       <UserProfile />
+//   //       <UpdateUser />
+//   //     </UserProvider>
+//   //   );
+// };
+// export default App;
+
+import UseReducer from "./hooks/useReducer/UseReducer";
+
+export default function App() {
   return (
-    <UserProvider>
-      <UserProfile />
-      <UpdateUser />
-    </UserProvider>
+    <>
+      <UseReducer />
+    </>
   );
-};
-export default App;
+}
