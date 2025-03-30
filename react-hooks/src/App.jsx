b0c20f021fa6dc711e6c7d781fb3481e8d8723ff
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-03-30 11:13:43 +0800

    useContext

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -2,12 +2,16 @@ import ComponentA from "./hooks/useContext/ComponentA";
 
 import { createContext } from "react";
 export const Data = createContext();
+export const Data1 = createContext();
 
 const App = () => {
   const name = "C_11nJxx";
+  const age = 21;
   return (
     <Data.Provider value={name}>
-      <ComponentA />
+      <Data1.Provider value={age}>
+        <ComponentA />
+      </Data1.Provider>
     </Data.Provider>
   );
 };
--- react-hooks/src/hooks/useContext/ComponentC.jsx
+++ react-hooks/src/hooks/useContext/ComponentC.jsx
@@ -1,12 +1,14 @@
-import { Data } from "../../App";
-
+import { Data, Data1 } from "../../App";
+import { useContext } from "react";
 const ComponentC = () => {
+  const name = useContext(Data);
+  const age = useContext(Data1);
   return (
-    <Data.Consumer>
-      {(name) => {
-        return <h1>{name}</h1>;
-      }}
-    </Data.Consumer>
+    <>
+      <h1>
+        My name is {name}, I' m {age} years old.
+      </h1>
+    </>
   );
 };
 
