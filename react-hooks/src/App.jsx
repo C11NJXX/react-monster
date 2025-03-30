10a8e11f22849052487ca65e2512e9046c6002f9
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-03-30 11:07:50 +0800

    useContext, createContext Provider Consumer

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,5 +1,15 @@
+import ComponentA from "./hooks/useContext/ComponentA";
+
+import { createContext } from "react";
+export const Data = createContext();
+
 const App = () => {
-  return <div>App</div>;
+  const name = "C_11nJxx";
+  return (
+    <Data.Provider value={name}>
+      <ComponentA />
+    </Data.Provider>
+  );
 };
 
 export default App;
