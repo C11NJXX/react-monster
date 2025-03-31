bedd14cd23fe2a77c7c85b3b1a71d8306f6a9f26
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-03-31 13:58:03 +0800

    useState<T>

--- react-ts-demo/src/main.tsx
+++ react-ts-demo/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 // import App from "./1. Component Props Typing/App";
 // import App from "./1. Component Props Typing/exercise/App";
+import App from "./5. useState Types/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
