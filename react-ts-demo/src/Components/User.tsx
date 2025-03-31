1dbf684919c88bf3bd7b42f39f7e9a07afc9d410
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-03-31 11:14:57 +0800

    destructing

--- react-ts-demo/src/Components/User.tsx
+++ react-ts-demo/src/Components/User.tsx
@@ -1,9 +1,17 @@
-const User = (props: { name: string; age: number; isStu: boolean }) => {
+const User = ({
+  name,
+  age,
+  isStu,
+}: {
+  name: string;
+  age: number;
+  isStu: boolean;
+}) => {
   return (
     <div>
-      <p>Name : {props.name}</p>
-      <p>Age : {props.age}</p>
-      <p>{props.isStu ? "Student" : "FreeLancer"}</p>
+      <p>Name : {name}</p>
+      <p>Age : {age}</p>
+      <p>{isStu ? "Student" : "FreeLancer"}</p>
     </div>
   );
 };
