c3f8927a3ac4fff76849c509370092b9aa2ed602
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-03-31 11:21:54 +0800

    pass children prop with type(ReactNode)

--- react-ts-demo/src/App.tsx
+++ react-ts-demo/src/App.tsx
@@ -1,9 +1,13 @@
+import Child from "./Components/Child";
 import User from "./Components/User";
 
 const App = () => {
   return (
     <div>
       <User name="C_11nJxx" age={21} isStu={true} />
+      <Child>
+        <p>I'm passed as a prop named "children" with type: ReactNode</p>
+      </Child>
     </div>
   );
 };
---
+++ react-ts-demo/src/Components/Child.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const Child = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default Child;
