ef9657552c9110acb5068c3464aa33d08f130cb3
C_11nJxx <c11njxxsmailbox@gmail.com>
2025-04-01 11:06:59 +0800

    Calendar

--- shadcn-ui-demo/src/App.tsx
+++ shadcn-ui-demo/src/App.tsx
@@ -1,10 +1,17 @@
-import { Button } from "./components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import React from "react";
 
 function App() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </>
   );
 }
 
